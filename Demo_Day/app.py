b77ac018b3ab52cf4c1773829aafe8fc5035d51c
Nikita <khosenag03@yahoo.com>
2019-10-24 14:54:35 -0400

    update

--- Demo_Day/app.py
+++ Demo_Day/app.py
@@ -14,25 +14,78 @@ def home():
     return render_template("dashboard.html")
 
 
-@app.route("/Dashboard2")
-def user():
+@app.route("/barchart")
+def Barcharts():
 
     # Return template and data
-    return render_template("dashboard2.html")
+    return render_template("Barchart.html")
 
-
-@app.route("/Table")
-def table():
-
-    # Return template and data
-    return render_template("table.html")
-
-@app.route("/Maps")
-def maps():
+@app.route("/code")
+def coding():
 
     # Return template and data
-    return render_template("maps.html")
+    return render_template("code.html")
 
+@app.route("/video")
+def Videos():
+
+    # Return template and data
+    return render_template("video.html")
+
+@app.route("/maps")
+def Maps():
+
+    # Return template and data
+    return render_template("Map.html")
+
+@app.route("/drugcost")
+def drgcosts():
+
+    # Return template and data
+    return render_template("Drugcost.html")
+
+
+@app.route("/frequent")
+def FP():
+
+    # Return template and data
+    return render_template("FruentlyP.html")
+
+@app.route("/insulins")
+def Insulin():
+
+    # Return template and data
+    return render_template("Insulins.html")
+
+@app.route("/tiotropium")
+def Tiotro():
+
+    # Return template and data
+    return render_template("Tiotropium.html")
+
+@app.route("/esomerprazole")
+def Esomerpra():
+
+    # Return template and data
+    return render_template("Esomerprazole.html")
+
+@app.route("/rouvastatin")
+def Rouvasta():
+
+    # Return template and data
+    return render_template("Rouvastatin.html")
+
+@app.route("/hydroco")
+def Narcotics():
+
+    # Return template and data
+    return render_template("Hydrocodone.html")
+
+@app.route("/opioids")
+def Opio():
+
+    # Return template and data
+    return render_template("Opioids.html")
 
 
 
